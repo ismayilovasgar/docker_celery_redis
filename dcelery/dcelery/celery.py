4ae5b4c8c665e068ff0c4e92830db5a9da3b054f
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2025-02-23 23:40:04 +0400

    save

--- dcelery/dcelery/celery.py
+++ dcelery/dcelery/celery.py
@@ -3,9 +3,11 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dcelery.settings")
 app = Celery("dcelery")
-app.conf.from_object("django.conf:settings", namespace="CELERY")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 
 @app.task
-def add_numbers(a, b):
-    return a + b
+def add_numbers():
+    return "@app.task-1"
+
+app.autodiscover_tasks()
