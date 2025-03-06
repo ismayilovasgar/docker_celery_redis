e00be502d01580f9f7a0a7f0b15aad9c0586493c
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2025-03-06 19:14:13 +0400

    save

--- command.md
+++ command.md
@@ -4,3 +4,21 @@ http://0.0.0.0:8000
 docker-compose up -d --build
 ./manage.py startapp taskapp
 docker exec -it django /bin/sh
+
+
+tp1.delay()
+
+from celery import group
+from newapp.tasks import *
+
+
+<!-- Group -->
+task_group = group(tp1.s(),tp2.s(),tp3.s(),tp4.s())
+task_group.apply_async() 
+
+result =  group(tp1.s(),tp2.s(),tp3.s(),tp4.s())()
+print(result.get()) # => [ "tp1","tp2","tp3","tp4" ]
+<!--  -->
+
+<!-- Chain -->
+task_chain = chain( tp1.s(),tp3.s(),tp2.s())
--- dcelery/newapp/tasks.py
+++ dcelery/newapp/tasks.py
@@ -1,9 +1,56 @@
 from celery import shared_task
+import time
+
+# @shared_task
+# def task1_dcelery():
+#     return "task-1-dcelery"
+
+# @shared_task
+# def task2_dcelery():
+#     return "task-2-dcelery"
+
+
+# @shared_task
+# def tp1():
+#     time.sleep(3)
+#     return
+
+# @shared_task
+# def tp2():
+#     time.sleep(3)
+#     return
+
+# @shared_task
+# def tp3():
+#     time.sleep(3)
+#     return
+
+# @shared_task
+# def tp4():
+#     time.sleep(3)
+#     return
+
+
+# ! way -1
+@shared_task(rate_limit="10/m")
+def tp1(queue="celery"):
+    time.sleep(3)
+    return "tp1"
+
 
 @shared_task
-def task1_dcelery():
-    return "task-1-dcelery"
+def tp2(queue="celery_1"):
+    time.sleep(3)
+    return "tp2"
+
 
 @shared_task
-def task2_dcelery():
-    return "task-2-dcelery"
+def tp3(queue="celery_2"):
+    time.sleep(3)
+    return "tp3"
+
+
+@shared_task
+def tp4(queue="celery_3"):
+    time.sleep(3)
+    return "tp4"
